ed24b8487a3809f8e352002c8b0ee90dc00b3b23
praveen G <37699745+Gpraveen12345@users.noreply.github.com>
2023-01-27 12:18:13 +0530

    implemented Change Detectioin onpush

--- src/app/employedetails/employedetails.component.ts
+++ src/app/employedetails/employedetails.component.ts
@@ -1,16 +1,26 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, NgForm } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 import { EmployeedetailsService } from '../employeedetails.service';
 import { ModeldialogeComponent } from '../modeldialoge/modeldialoge.component';
 
 @Component({
   selector: 'app-employedetails',
   templateUrl: './employedetails.component.html',
-  styleUrls: ['./employedetails.component.scss']
+  styleUrls: ['./employedetails.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployedetailsComponent implements OnInit {
-  constructor(public dialog: MatDialog, private employeeDetailsService: EmployeedetailsService) {
+  public heading=" Create a new employee";
+  public subheading="try this out"
+  constructor(public dialog: MatDialog, private employeeDetailsService: EmployeedetailsService,public cdr: ChangeDetectorRef) {
+  setTimeout(() => {
+    this.heading="change Dectection strategy";
+    console.log("change dectection strategy should trigger");
+    // cdr.detectChanges(); //This will make the change dectection strategy to mark for the change
+  }, 1000);
   }
   ngOnInit() {
     this.employeeDetailsService.observable.subscribe(
